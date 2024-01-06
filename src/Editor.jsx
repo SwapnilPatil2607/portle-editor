5e6720eebe3da918cd08ddbe945d5994134d1051
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2024-01-06 11:18:16 +0530

    setting up basic functionality for current block

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,21 +1,37 @@
-import { EditorState } from "draft-js";
-import "draft-js/dist/Draft.css";
 import { useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState, convertToRaw, RichUtils } from "draft-js";
+import "draft-js/dist/Draft.css";
 
 function MyEditor() {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
+  const onEditorStateChange = (editorState) => {
+    console.log(convertToRaw(editorState.getCurrentContent()));
+
+    const contentState = editorState.getCurrentContent();
+    const selection = editorState.getSelection();
+    const key = selection.getStartKey();
+    const block = contentState.getBlockForKey(key);
+
+    console.log(block.getText());
+
+    const newBlock = block.set("type", "header-one");
+    const newContentState = contentState.merge({
+      blockMap: contentState.getBlockMap().set(key, newBlock),
+    });
+
+    setEditorState(
+      EditorState.push(editorState, newContentState, "change-block-type")
+    );
+  };
 
   return (
     <Editor
       editorState={editorState}
-      toolbarClassName="toolbarClassName"
-      wrapperClassName="wrapperClassName"
-      editorClassName="editorClassName"
-      onEditorStateChange={setEditorState}
+      onEditorStateChange={onEditorStateChange}
     />
   );
 }
