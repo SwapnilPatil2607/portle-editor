125d51dd52ecee8cce3082a91b33bb7f54bab995
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2024-01-06 09:49:35 +0530

    react-draft-wysiwyg library added

---
+++ src/Editor.jsx
@@ -0,0 +1,23 @@
+import { EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
+import { useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+function MyEditor() {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  return (
+    <Editor
+      editorState={editorState}
+      toolbarClassName="toolbarClassName"
+      wrapperClassName="wrapperClassName"
+      editorClassName="editorClassName"
+      onEditorStateChange={setEditorState}
+    />
+  );
+}
+
+export default MyEditor;
