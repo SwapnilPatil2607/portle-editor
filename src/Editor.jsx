0860ff08c4e3e5e3e2e0e899df852e34c065873c
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2024-01-06 12:09:52 +0530

    feat: changing text to header when '# ' pressed

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { EditorState, convertToRaw, RichUtils } from "draft-js";
+import { EditorState, convertToRaw, Modifier } from "draft-js";
 import "draft-js/dist/Draft.css";
 
 function MyEditor() {
@@ -12,20 +12,29 @@ function MyEditor() {
     console.log(convertToRaw(editorState.getCurrentContent()));
 
     const contentState = editorState.getCurrentContent();
+
     const selection = editorState.getSelection();
     const key = selection.getStartKey();
     const block = contentState.getBlockForKey(key);
 
     console.log(block.getText());
+    const headerType = block.getText().split("# ").length === 2;
+    console.log(headerType);
 
-    const newBlock = block.set("type", "header-one");
-    const newContentState = contentState.merge({
-      blockMap: contentState.getBlockMap().set(key, newBlock),
-    });
+    if (headerType) {
+      const makeHeader = block.set("type", "header-one");
+      const newBlock = makeHeader.set("text", block.getText().split("# ")[1]);
 
-    setEditorState(
-      EditorState.push(editorState, newContentState, "change-block-type")
-    );
+      const newContentState = contentState.merge({
+        blockMap: contentState.getBlockMap().set(key, newBlock),
+      });
+
+      setEditorState(
+        EditorState.push(editorState, newContentState, "change-block-type")
+      );
+    } else {
+      setEditorState(editorState);
+    }
   };
 
   return (
