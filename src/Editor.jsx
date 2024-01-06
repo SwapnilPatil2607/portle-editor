2c53e000acec26bd0274a4b9ee97b5dde8692570
SwapnilPatil2607 <swapnilpatil2607@gmail.com>
2024-01-06 14:41:58 +0530

    feat: red color text condition added

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { EditorState, convertToRaw, Modifier, SelectionState } from "draft-js";
+import { EditorState, Modifier } from "draft-js";
 import "draft-js/dist/Draft.css";
 
 function MyEditor() {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
+
   const onEditorStateChange = (editorState) => {
     const contentState = editorState.getCurrentContent();
 
@@ -15,9 +16,19 @@ function MyEditor() {
     const key = selection.getStartKey();
     const block = contentState.getBlockForKey(key);
 
-    const headerType = block.getText().split("# ").length === 2;
-    const boldType = block.getText().split("* ").length === 2;
-
+    const headerType =
+      block.getText().split(" ")[0] === "#" &&
+      block.getText().split(" ").length >= 2;
+    const boldType =
+      block.getText().split(" ")[0] === "*" &&
+      block.getText().split(" ").length >= 2;
+    const colorRedText =
+      block.getText().split(" ")[0] === "**" &&
+      block.getText().split(" ").length >= 2;
+    const underline =
+      block.getText().split(" ")[0] === "***" &&
+      block.getText().split(" ").length >= 2;
+    console.log(headerType, boldType, underline);
     if (headerType) {
       const makeHeader = block.set("type", "header-one");
       const newBlock = makeHeader.set("text", block.getText().split("# ")[1]);
@@ -42,23 +53,65 @@ function MyEditor() {
         "BOLD"
       );
 
-      // Create a new EditorState with the modified content
       const newEditorState = EditorState.push(
         editorState,
         newContentState,
         "bold-current-block"
       );
 
-      // Update the state with the new EditorState
       setEditorState(newEditorState);
+    } else if (underline) {
+      const newBlock = block.set("text", block.getText().split("*** ")[1]);
+      const newContentState = Modifier.applyInlineStyle(
+        contentState.merge({
+          blockMap: contentState.getBlockMap().set(key, newBlock),
+        }),
+        selection.merge({
+          anchorOffset: 0,
+          focusOffset: block.getLength(),
+        }),
+        "UNDERLINE"
+      );
+
+      const newEditorState = EditorState.push(
+        editorState,
+        newContentState,
+        "underline-current-block"
+      );
+
+      setEditorState(newEditorState);
+    } else if (colorRedText) {
+      const newContentState = Modifier.applyInlineStyle(
+        contentState,
+        selection.merge({
+          anchorOffset: 0,
+          focusOffset: block.getLength(),
+        }),
+        "red"
+      );
+
+      const newEditorStateWithRedText = EditorState.push(
+        editorState,
+        newContentState,
+        "red-text-current-block"
+      );
+
+      setEditorState(newEditorStateWithRedText);
     } else {
       setEditorState(editorState);
     }
   };
 
+  const customStyleMap = {
+    COLOR_RED: {
+      color: "red",
+    },
+  };
+
   return (
     <Editor
       editorState={editorState}
+      customStyleMap={customStyleMap}
       onEditorStateChange={onEditorStateChange}
     />
   );
